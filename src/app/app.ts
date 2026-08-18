907acfbdbbdafcc011dce5dfba0fe2a6e60670b7
UgurKulaksiz <ugur.kulaksiz@outlook.com>
2026-08-18 17:30:19 +0200

    Intégration des Signals Angular et stratégie OnPush
    - Utilisation de signal(), update() et computed() pour la gestion de l'item sélectionné
    - Ajout d'un effect() pour le logging des changements de valeur
    - Passage des composants en ChangeDetectionStrategy.OnPush pour le mode zoneless

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, signal } from '@angular/core';
 import { CollectionItemCard } from './components/collection-item-card/collection-item-card';
 import { CollectionItem } from './models/collection-item';
 import { SearchBar } from './components/search-bar/search-bar';
@@ -7,7 +7,8 @@ import { SearchBar } from './components/search-bar/search-bar';
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [CollectionItemCard, SearchBar]
+  imports: [CollectionItemCard, SearchBar],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
   searchText = '';
@@ -15,6 +16,18 @@ export class App {
 
   coin!: CollectionItem;
   linx!: CollectionItem;
+
+  itemList: CollectionItem[] = [];
+  // Création du Signal qui va contenir l'index à afficher
+  selectedItemIndex = signal(0);
+  // Création d'un Signal 'computed'
+  selectedItem = computed(() => {
+    return this.itemList[this.selectedItemIndex()];
+  });
+
+  logEffect = effect(() => {
+    console.log(this.selectedItemIndex(), this.selectedItem());
+  });
 
   constructor() {
     this.coin = new CollectionItem();
@@ -25,10 +38,17 @@ export class App {
     this.coin.price = 170;
 
     this.linx = new CollectionItem();
+
+    this.itemList = [this.coin, this.linx];
   }
 
   increamentCount() {
     this.count++;
   }
 
+  incrementIndex() {
+    this.selectedItemIndex.update((currentValue) => {
+      return (currentValue + 1) % 2; // %2 pour permettre de switcher entre les éléments 0 & 1
+    });
+  }
 }
